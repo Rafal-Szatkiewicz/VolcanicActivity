36219cbd89210ff64a3417802fc85c934a889da3
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2022-05-02 15:02:42 +0200

    uninstalled converter

--- convert.js
+++
@@ -1,8 +0,0 @@
-const csvToJson = require('convert-csv-to-json');
- 
-const input = './eruptions_left_outer.csv'; 
-const output = './public/volcanoEruptions.json';
- 
-csvToJson.fieldDelimiter('#')
-         .formatValueByType()
-         .generateJsonFileFromCsv(input, output);
